1a6314cdd70a0a8583ae504c1d74f18a473418bd
GersonSales <gerson.junior@ccc.ufcg.edu.br>
2018-08-29 20:38:34 -0300

    create db logic

--- db.js
+++ db.js
@@ -6,7 +6,32 @@ let db = null;
 
 module.exports = app => {
   if (!db) {
+    const config = app.libs.config;
+    const sequelize = new Sequelize(
+      config.database,
+      config.username,
+      config.password,
+      config.params
+    );
 
+    db = {
+      sequelize,
+      sequelize,
+      models : {}
+    };
+
+    const dir = path.join(__dirname, "models");
+    fs.readdirSync(dir).forEach(file => {
+      const modelDir = path.join(dir, file);
+      const model = sequelize.import(modelDir);
+      db.models[model.name] = model;
+    });
+
+    Object.keys(db.models).forEach(key => {
+      if (db.models[key].hasOwnProperty('associate')) {
+        db.models[key].associate(db.models);
+      }
+    });
   }
   return db;
 };
