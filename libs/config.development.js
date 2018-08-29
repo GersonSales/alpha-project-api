8b374234bc570b1dc5fa5082ade4f0c7b8d7320a
GersonSales <gerson.junior@ccc.ufcg.edu.br>
2018-08-29 20:20:22 -0300

    create the segregation of config files

---
+++ libs/config.development.js
@@ -0,0 +1,11 @@
+module.exports = {
+  database:"alpha-project",
+  username: "",
+  password: "",
+  params: {
+    dialect: "sqlite",
+    storage: "alpha_project.sqlite"
+  },
+  jwtSecret: "4lph4_pr0j3ct",
+  jwtSession: {session: false}
+};
