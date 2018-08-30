5bc3272698b03eb1af0f1c9824f00f8500b1fbfc
GersonSales <gerson.junior@ccc.ufcg.edu.br>
2018-08-29 21:33:51 -0300

    typing db fields

--- models/users.js
+++ models/users.js
@@ -2,11 +2,38 @@ const bcrypt = require('bcrypt');
 
 module.exports = (sequelize, DataType) => {
   let Users = sequelize.define("Users", {
-    id: {},
-    name: {},
-    email: {},
-    password:{},
-    isAdmin: {}
+    id: {
+      type: DataType.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    name: {
+      type: DataType.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true
+      }
+    },
+    email: {
+      type: DataType.STRING,
+      unique: true,
+      allowNull: false,
+      validate: {
+        notEmpty: true
+      }
+    },
+    password:{
+      type: DataType.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true
+      }
+    },
+    isAdmin: {
+      type: DataType.BOOLEAN,
+      allowNull: false,
+      defaultValue: false
+    }
   });
   Users.beforeCreate(user => {});
   Users.isPassword = (encodePassword, password) => {return true};
