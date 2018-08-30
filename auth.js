3ddef881f7436a833d0c1622370727f257c57651
GersonSales <gerson.junior@ccc.ufcg.edu.br>
2018-08-29 21:58:14 -0300

    create the authentication initial logic

--- auth.js
+++ auth.js
@@ -7,13 +7,37 @@ const Strategy = passportJWT.Strategy,
 module.exports = app => {
   const Users = app.db.models.Users;
   const config = app.libs.config;
-  const params = {};
+  const params = {
+    secretOrKey: config.jwtSecret,
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken('jwt')
+  };
 
-  const strategy = new Strategy();
+  const strategy = new Strategy(params, (payload, done) => {
+    Users
+      .findById((payload.id))
+      .then(user => {
+        if(user) {
+          return done(null, {
+            id: user.id,
+            email: user.email
+          });
+        }
+          done(new Error("User not found"), null);
+      })
+      .catch(error => {
+        done(error, null);
+      })
+  });
+
+  passport.use(strategy);
 
   return {
-    initialize: () => {},
-    authenticate: () => {}
+    initialize: () => {
+      return passport.initialize()
+    },
+    authenticate: () => {
+      return passport.authenticate('jwt', config.jwtSession);
+    }
   }
 
 
