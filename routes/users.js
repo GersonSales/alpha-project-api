54113b2e8ddecba8f98fe26ca55e32b18d24df62
GersonSales <gerson.junior@ccc.ufcg.edu.br>
2018-08-30 01:42:50 -0300

    create the initial logic of the rest crud with authentication

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,24 @@
 module.exports = app => {
   const Users = app.db.models.Users;
+
+  app.route('/user')
+    .all(app.auth.authenticate())
+
+    .put((req, res) => {
+      console.log("PUT");
+      res.sendStatus(204);
+    })
+
+    .get((req, res) => {
+      console.log("GET");
+      res.sendStatus(204);
+    })
+
+    .delete((req, res) => {
+      console.log("DELETE");
+      res.sendStatus(204);
+    });
+
   app.post('/users', (req, res) => {
     Users.create(req.body)
       .then(result => {
