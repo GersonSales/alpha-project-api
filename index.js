c3e72a574cf2f47efb12295a2ed5eab4e98e8fdf
GersonSales <gerson.junior@ccc.ufcg.edu.br>
2018-08-29 22:33:09 -0300

    add the middleware dependency

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const express = require('express'),
   app = express();
 
 consign({verbose : false})
-  .include('libs/middlewares.js')
+  .include('libs/config.js')
+  .then('db.js')
+  .then('auth.js')
+  .then('libs/middlewares.js')
   .then('routes')
   .then('/libs/boot.js')
   .into(app);
+
+module.exports = app;
