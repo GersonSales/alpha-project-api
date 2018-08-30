29618ee8a7c346295140ebd706e7133dfe652d0b
GersonSales <gerson.junior@ccc.ufcg.edu.br>
2018-08-30 03:35:31 -0300

    load necessary module only if isn't in the test mode

--- libs/boot.js
+++ libs/boot.js
@@ -1,7 +1,9 @@
 module.exports = app => {
-  app.db.sequelize.sync().done(() => {
-    app.listen(app.get('port'), () => {
-      console.log(`Aplha Project is listen on ${app.get('port')}`);
+  if (process.env.NODE_ENV !== "test") {
+    app.db.sequelize.sync().done(() => {
+      app.listen(app.get('port'), () => {
+        console.log(`Aplha Project is listen on ${app.get('port')}`);
+      });
     });
-  });
+  }
 };
