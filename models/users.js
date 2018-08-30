d4fcfab0091e7875a53dc4a80fda1a2b9a6067d1
GersonSales <gerson.junior@ccc.ufcg.edu.br>
2018-08-29 21:10:34 -0300

    initial definition of user model fields

--- models/users.js
+++ models/users.js
@@ -1,7 +1,13 @@
 const bcrypt = require('bcrypt');
 
 module.exports = (sequelize, DataType) => {
-  let Users = sequelize.define("Users", {});
+  let Users = sequelize.define("Users", {
+    id: {},
+    name: {},
+    email: {},
+    password:{},
+    isAdmin: {}
+  });
   Users.beforeCreate(user => {});
   Users.isPassword = (encodePassword, password) => {return true};
   Users.associate = (models) => {};
