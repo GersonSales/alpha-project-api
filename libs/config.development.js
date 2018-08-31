49b011d0ecd7255989b28cd6ed53bd98ceed1afc
GersonSales <gerson.junior@ccc.ufcg.edu.br>
2018-08-30 23:40:19 -0300

    bind the sql logger to the local one

--- libs/config.development.js
+++ libs/config.development.js
@@ -1,10 +1,14 @@
+const logger = require("./logger.js");
 module.exports = {
   database:"alpha-project",
   username: "",
   password: "",
   params: {
     dialect: "sqlite",
-    storage: "alpha_project.sqlite"
+    storage: "alpha_project.sqlite",
+    logging: ((sql) => {
+      logger.info(`[${new Date}] ${sql}`);
+    })
   },
   jwtSecret: "4lph4_pr0j3ct",
   jwtSession: {session: false}
