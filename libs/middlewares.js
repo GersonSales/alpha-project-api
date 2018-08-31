967f7d55e6f7d79fa2beec359b8377604d3279e2
GersonSales <gerson.junior@ccc.ufcg.edu.br>
2018-08-30 23:41:01 -0300

    create the middleware for the morgan logger

--- libs/middlewares.js
+++ libs/middlewares.js
@@ -1,7 +1,17 @@
-const bodyParser = require("body-parser");
+const bodyParser = require("body-parser"),
+  morgan = require("morgan"),
+  logger = require("./logger.js");
 
 module.exports = app => {
   app.set("port", 3000);
   app.set("json spaces", 4);
+  app.use(morgan("common", {
+    stream: {
+      write: (message) => {
+        logger.info(message)
+      }
+    }
+  }));
+
   app.use(bodyParser());
 };
