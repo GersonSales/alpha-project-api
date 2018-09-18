752763017ca5c2fde749d38cdf23c1135433dfa5
GersonSales <gerson.junior@ccc.ufcg.edu.br>
2018-09-18 00:47:09 -0300

    create the db connection and initial user schema

--- app.js
+++ app.js
@@ -2,10 +2,12 @@
 
 const express = require("express");
 const consign = require("consign");
+
 const app = express();
 consign()
     .include("config/middleware.js")
     .then("src/router.js")
     .then("config/boot.js")
     .into(app);
+
 module.exports = app;
--- config/middleware.js
+++ config/middleware.js
@@ -1,5 +1,16 @@
 "use strict";
-module.exports = app => {
-   app.set("port", 3000);
+
+const mongoose = require("mongoose");
+const userSchema = require("./../src/user/schema");
+
+module.exports = async (app) => {
+  try {
+    app.set("port", 3000);
+    app.use(userSchema);
+
+    await mongoose.connect("mongodb://root:alph4root@ds261302.mlab.com:61302/alpha_project_db", { useNewUrlParser: true } );
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 
