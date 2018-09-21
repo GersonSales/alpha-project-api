296b67659c8e5de6f74b4597261b7c97bf2d99a1
GersonSales <gerson.junior@ccc.ufcg.edu.br>
2018-09-21 20:58:16 -0300

    solve codacy pr issues

--- config/config.js
+++ config/config.js
@@ -5,7 +5,7 @@ global.SALT_KEY = "temp-key";
 module.exports = (app) => {
   const env = process.env.NODE_ENV;
   if(env) {
-    return require(`./config.${env}`);
+    return require("./config." + env);
   }else {
     return require("./config.development");
   }
