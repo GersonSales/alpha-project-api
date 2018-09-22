fe0502046490aabbb08828fd50672257c751a006
GersonSales <gerson.junior@ccc.ufcg.edu.br>
2018-09-22 01:49:38 -0300

    fix the test configuration and remove server starts from it

--- src/test/index/route.js
+++ src/test/index/route.js
@@ -1,11 +1,11 @@
 describe("Routes: Index", () => {
   describe("GET /", () => {
-    it("returns the API status", done => {
+    it("returns the API status", (done) => {
       request
         .get('/')
         .expect(200)
         .end((error, res) => {
-          const expected = {response: "Welcome to the Alpha Project"};
+          const expected = "Welcome to the Alpha Project API";
           expect(res.body).to.eql(expected);
           done(error);
         });
