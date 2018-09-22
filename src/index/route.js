93bdfaef8b03cb85b8f963911202f4532f649e3f
GersonSales <gerson.junior@ccc.ufcg.edu.br>
2018-09-21 21:24:45 -0300

    create and configure the initial app tests

--- src/index/route.js
+++ src/index/route.js
@@ -4,7 +4,7 @@ const express = require("express");
 const router = new express.Router();
 
 router.get("/", (req, res) => {
-        res.status(200).json({response : "Welcome to the Alpha project"});
+        res.status(200).json({response : "Welcome to the Alpha Project"});
 });
 
 module.exports = router;
