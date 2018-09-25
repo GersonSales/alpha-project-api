3dc53bf6248dc18491fd8214d7625cdc8ae43af2
GersonSales <gerson.junior@ccc.ufcg.edu.br>
2018-09-25 03:32:58 -0300

    create infimous changes

--- config/boot.js
+++ config/boot.js
@@ -4,7 +4,7 @@ module.exports = (app) => {
   if (process.env.NODE_ENV !== "test") {
     const port = app.get("port");
     app.listen(port, () => {
-      console.log("Api project is listening on port " + port);
+      console.log("Api project hr is listening on port " + port);
     });
   }
 }
