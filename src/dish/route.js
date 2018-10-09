504f64871760b95f240a070a39f0cf6b9bd9b9e7
GersonSales <gerson.junior@ccc.ufcg.edu.br>
2018-10-09 02:40:43 -0300

    create dishes doccumentation

--- src/dish/route.js
+++ src/dish/route.js
@@ -4,9 +4,11 @@ const express = require("express");
 const router = new express.Router();
 const controller = require("./controller");
 const auth = require("./../auth/service");
+const authenticate = auth.authenticate;
+const authorizeByRole = auth.authorizeByRole;
 
 router.get("/", controller.get);
-router.post("/", auth.authorizeByRole, controller.create);
+router.post("/", authenticate, authorizeByRole, controller.create);
 
 router.put("/:id",  controller.edit);
 router.delete("/:id", controller.delete);
