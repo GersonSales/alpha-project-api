08f38f97d5ff2f9651090491429a6121a3fb3a25
GersonSales <gerson.junior@ccc.ufcg.edu.br>
2018-12-05 04:21:27 -0300

    logging requests

--- src/dish/controller.js
+++ src/dish/controller.js
@@ -2,11 +2,14 @@
 
 const repository = require("./repository");
 
-exports.get= async (req, res) => {
+exports.get = async (req, res) => {
+  console.log("Trying to get dishes list.");
   try {
     const result = await repository.find(req.query);
+    console.log("Got a dish list with size " + result.length);
     res.status(200).json({result});
   } catch (error) {
+    console.log("Error while processing get list request. ErrorMessage: " + error.message);
     res.status(500).json({errorMessage: error.message});
   }
 };
@@ -16,16 +19,19 @@ exports.edit = async (req, res) => {
     const dishId = req.params.id;
     await repository.findByIdAndUpdate(dishId, req.body);
     res.status(200).send();
-  }catch (error) {
+  } catch (error) {
     res.status(500).json({errorMessage: error.message});
   }
 };
 
 exports.create = async (req, res) => {
+  console.log("Trying to create a new dish.");
   try {
     await repository.create(req.body);
+    console.log("Dish successfully created.");
     res.status(201).send();
   } catch (error) {
+    console.log("Error while creating a new dish. ErrorMessage: " + error.message);
     res.status(500).json({errorMessage: error.message});
   }
 };
